2fb8d268d8797f013563b1f3c9525e43395c4462
Sunandan Bose <sunandan.riders@gmail.com>
2020-05-09 00:32:25 +0530

    Restricted player movement area and added player rotation feature

--- renderer.js
+++ renderer.js
@@ -12,28 +12,56 @@ window.addEventListener('keydown', function(event) {
 	let x = player.style.left;
 	let y = player.style.top;
 	if(event.keyCode == 37) {
-
-		player.style.left = distanceToMove(x,-10);
+		player.style.left = distanceToMove(x,-10,'Left');
+		player.style.transform ='rotate(180deg)';
 		document.getElementById('log').innerHTML = 'Left was pressed again'
 	};
 	if(event.keyCode == 38) {
-		player.style.top= distanceToMove(y,-10);
+		player.style.top= distanceToMove(y,-10,'Up');
+		player.style.transform ='rotate(270deg)';
 		document.getElementById('log').innerHTML = 'Up was pressed again'
 	};
 	if(event.keyCode == 39) {
-		player.style.left= distanceToMove(x,10);
+		player.style.left= distanceToMove(x,10,'Right');
+		player.style.transform ='rotate(0deg)';
 		document.getElementById('log').innerHTML = 'Right was pressed again'
 	};
 	if(event.keyCode == 40) {
-		player.style.top= distanceToMove(y,10);
+		player.style.top= distanceToMove(y,10,'Down');
+		player.style.transform ='rotate(90deg)';
 		document.getElementById('log').innerHTML = 'Down was pressed again'
 	};
 });
 
-function distanceToMove(actualValue, toMove){
+function distanceToMove(actualValue, toMove, Direction){
+	if(!boundaryCheck(toMove,Direction))
+		return actualValue;
 	if(actualValue === "")
 		return toMove + 'px';
 	else{
 		return (Number(actualValue.substring(0,actualValue.length -2)) + toMove )+ 'px';
 	}
+}
+
+function boundaryCheck(toMove, Direction){
+	let player = document.getElementById("player");
+	let left = player.style.left;
+	let top = player.style.top;
+	if(Direction === 'Left'){
+		if(left === "" || left === "0px")
+			return false;
+	}
+	if(Direction === 'Up'){
+		if(top === "" || top === "0px")
+			return false;
+	}
+	if(Direction === 'Right'){
+		if(left === "560px")
+			return false;
+	}
+	if(Direction === 'Down'){
+		if(top === "350px")
+			return false;
+	}
+	return true;
 }
